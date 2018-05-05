4d55f14687efe3ef35cad44bf6d44800e0b31cce
Michael O'Sullivan <michael@nizamimedia.com>
2018-05-05 23:56:08 +0100

    Event listender functionality
    Added and removed bootstrap due to conflick.
    Added in all variables
    Created prevent page reload on submit button click.
    Set default load html colour picker colour.
    Updated HTML to include the onclick and onchange listening events.
    Created myColour function to store the colour variable onchange.
    Created make grid function to store the sizes from the user input.
    Everything is currently displaying to the console.log.

--- designs.js
+++ designs.js
@@ -1,10 +1,41 @@
 // Select color input
 // Select size input
 
+let inputHeight, inputWeight, color;
+
 // When size is submitted by the user, call makeGrid()
+
+//This block disables the page reloading or default submit functionality
+
+document.getElementById("submit").addEventListener("click", function(event){
+    event.preventDefault()
+});
+
+
+//Setting the default colour as a JS variable onload. Rather than take it from the picker we can defauly to block as it resets to black on apge loads
+color = '#000000';
+console.log(color);
+
+
+//This function loads the value of the colour picker and is run on event change via the HTML
+
+function myColour(){
+	color = document.getElementById('colorPicker').value;
+	console.log(color)
+}
+
+
 
 function makeGrid() {
 
-// Your code goes here!
+	inputHeight = document.getElementById('inputHeight').value;
+	inputWeight = document.getElementById('inputWeight').value;
+
+	inputHeight = parseInt(inputHeight);
+
+	inputWeight = parseInt(inputWeight);
+
+	console.log(inputHeight + " " + inputWeight);
 
 }
+
